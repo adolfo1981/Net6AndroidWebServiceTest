ea0ba6bedbc6d56afb6d65c63ee68e6d9b8e6d26
Adolfo Perez <aperez@fingercheck.com>
2022-08-03 14:03:23 -0500

    Adding Web Service Server and Client projects

--- HelloAndroid/MainActivity.cs
+++ HelloAndroid/MainActivity.cs
@@ -2,18 +2,29 @@
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using ServiceReference1;
 
 namespace HelloAndroid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            try
+            {
+                var soapClient = new WebServiceSoapClient(WebServiceSoapClient.EndpointConfiguration.WebServiceSoap);
+                var helloResponse = await soapClient.HelloWorldAsync();
+            }
+            catch (System.Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
