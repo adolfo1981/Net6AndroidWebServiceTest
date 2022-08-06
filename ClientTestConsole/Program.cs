aa38b4c1757ba279a72ee7e8474a6efd50f51fc0
Adolfo Perez <aperez@fingercheck.com>
2022-08-06 13:56:11 -0500

    Adding Test Console project which works consuming Web Service through Web Service Client project

---
+++ ClientTestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ServiceReference1;
+
+//Console.WriteLine("Hello, World!");
+
+try
+{
+    var soapClient = new WebServiceSoapClient(WebServiceSoapClient.EndpointConfiguration.WebServiceSoap);
+    //Sync test
+    //var helloResponse = soapClient.HelloWorld();
+    //Async test
+    var helloResponse = await soapClient.HelloWorldAsync();
+    Console.WriteLine($"SoapClient HelloWorldAsync Response: {helloResponse.Body.HelloWorldResult}");
+}
+catch (System.Exception ex)
+{
+    throw;
+}
